602a8a82f7feb5df48078305057a0d5aca999257
Chunyou Peng <chunyoupeng@gmail.com>
2025-09-02 22:21:08 +0800

    数据增强改变

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 yolo detect train \
   data=/root/autodl-tmp/three_data/merged_dataset/data.yaml \
   model=yolov8x.pt epochs=100 imgsz=640 batch=16 device=0 \
-  augment=True \
-  mosaic=1.0 close_mosaic=10 \
-  mixup=0.15 copy_paste=0.05 \
-  hsv_h=0.015 hsv_s=0.7 hsv_v=0.4 \
-  degrees=7.5 translate=0.10 scale=0.50 shear=2.0 perspective=0.0005 \
-  fliplr=0.5 flipud=0.0 \
-  erasing=0.2
+  augment=True multi_scale=True \
+  mosaic=0.8 close_mosaic=15 \
+  mixup=0.10 copy_paste=0.05 \
+  hsv_h=0.015 hsv_s=0.50 hsv_v=0.35 \
+  degrees=3.0 translate=0.05 scale=0.40 shear=1.0 perspective=0.001 \
+  fliplr=0.0 flipud=0.0 \
+  erasing=0.1
